76abba52750063677cc3ece22046597d64e67fc3
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-22 12:07:11 -0800

    Make a list

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
+  const int LIST_SIZE = 5;
+
 do
 {
 
@@ -17,7 +20,22 @@ do
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+ if( input == 'a' || input == 'A')
+ {
+    if( numItems < LIST_SIZE )
+    {
+        cout<<"What is the item?"<<endl;
+        cin>>list[numItems];
+        numItems++;
+    }
+    else
+    {
+        cout<<"You'll need a bigger list!"<<endl;
+    }
+ }
+
 } while(input != 'q' && input != 'Q');
 
+ 
   return 0;
 }
