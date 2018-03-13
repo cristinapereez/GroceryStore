9a26d7122651bf3af51a3349cdb1ce2246f22f65
cristinapereez <cperez58@mail.csuchico.edu>
2018-03-13 12:42:31 -0700

    Change to vector

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 //Author: Cristina Perez
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
+  vector <string> list; //array of 5 strings
+  int numItems;
   char input;
   string item;
   
@@ -26,29 +27,41 @@ do
  {
      cout<<"What is the item?"<<endl;
      cin>>item;
-     int len = item.length();
-    if( numItems < 5 )
-    {
-        list[numItems] = item;
-        numItems++;       
-    }
-    else
-    {
-        cout<<"You'll need a bigger list!"<<endl;
-        
-    }
- }
+     list.push_back(item);
+       
+}
 
 } while(input != 'q' && input != 'Q' );
 
-if( input == 'q' || input == 'Q')
-{
-   cout<<"==ITEMS TO BUY==";
+
+/*   cout<<"==ITEMS TO BUY==";
    cout<<"\n1 "<<list[0];
    cout<<"\n2 "<<list[1];
    cout<<"\n3 "<<list[2];
    cout<<"\n4 "<<list[3];
-   cout<<"\n5 "<<list[4]<<endl;
-} 
+   cout<<"\n5 "<<list[4]<<endl
+*/
+
+if( input == 'q' || input == 'Q')
+{
+ if( list.empty() == true )
+    {
+      cout<<"No items to buy!"<<endl;       
+    }
+    else
+    {
+     cout<<"==ITEMS TO BUY=="<<endl;
+
+    for(int i = 0; i < list.size(); i++)
+    {
+     cout<<i+1<<" "<<list[i]<<endl;
+    }
+    }
+
+
+}  
+    
+ 
+
   return 0;
 }
